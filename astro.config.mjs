e7b302b5ae2b01a4e6333c2da19ef447e4ec1b07
Diego Reyes Cabrera <diegoreyes150103@gmail.com>
2024-11-09 08:00:27 -0500

    feat: add personal-intro section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import svgr from "vite-plugin-svgr";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({ applyBaseStyles: false }),
+    react(),
+  ],
+  vite: {
+    plugins: [svgr()],
+  },
 });
